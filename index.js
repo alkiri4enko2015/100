8f2f4a8a2a639b5bc128465cba131addf2385262
alkiri4enko2015 <bloxy.net@yandex.ru>
2017-05-07 04:34:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-var fs = require('fs');
+/*var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 8081;
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/index', function (req, res) {
+    res.render('pages/index');
+});
+app.get('/controllers', function (req, res) {
+    res.render('pages/controller');
+});
+
+io.on('connection', function (socket) {
+    socket.on('comand_i', function (data) {
+        data.socket_id = socket.id;
+        socket.broadcast.emit('comand_o', data);
+    });
+
+    socket.on('disconnect', function(){
+        socket.broadcast.emit('comand_o', {
+            socket_id: socket.id,
+            command: 'delete'
+        });
+    });
+});
+
+server.listen(port, function () {
+    console.log(`App listening on port ${port}`);
+});*/
 
 var express = require("express");
 var err = express();
+var app = require('http').createServer(err);
+var io = require('socket.io')(app);
+var fs = require('fs');
 
 var port = process.env.PORT || 5000;
 app.listen(port,function() {
   console.log("Listening on " + port);
 });
 
+err.set('views', __dirname + '/views');
+err.set('view engine', 'ejs');
+
+err.get('/index', function (req, res) {
+    res.render('pages/index');
+});
+err.get('/controllers', function (req, res) {
+    res.render('pages/controller');
+});
+
 function handler (req, res) {
-  fs.readFile(__dirname + '/public/index.html',
+  fs.readFile(__dirname + '/views/pages/index.ejs',
   function (err, data) {
     if (err) {
       res.writeHead(500);
@@ -22,22 +66,17 @@ function handler (req, res) {
     res.end(data);
   });
   }
-  
-
+ 
 io.on('connection', function (socket) {
-  console.log('client connect');
-  socket.on('my other event', function (data) {
- 		socket.emit('news', data);
-  });
-});
-
-/*err.use(express.static(__dirname + "/public"));
-
-err.get("/", function(request, response){   
-    	//response.end(__dirname+'/public/index.html');
+  socket.on('comand_i', function (data) {
+        data.socket_id = socket.id;
+        socket.broadcast.emit('comand_o', data);
     });
 
-var port = process.env.PORT || 5000;
-err.listen(port, function() {
-  console.log("Listening on " + port);
-});*/
+    socket.on('disconnect', function(){
+        socket.broadcast.emit('comand_o', {
+            socket_id: socket.id,
+            command: 'delete'
+        });
+    });
+});
